79993b281327a5dd5a9bc92e907bed8806f992ef
noob <noobie37@protonmail.com>
2021-06-18 17:47:16 +0530

    Updates

---
+++ precache-manifest.b7c34c70ea9e2e3e602485463b684637.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1eb7b0f9c2d9756e0be43ce1b0841b",
+    "url": "/tic-tac-toe-game/index.html"
+  },
+  {
+    "revision": "fd8455a4bcf3d5006b8e",
+    "url": "/tic-tac-toe-game/static/js/2.c95199ad.chunk.js"
+  },
+  {
+    "revision": "048208f9d821be3b6bf5",
+    "url": "/tic-tac-toe-game/static/js/main.103d8569.chunk.js"
+  },
+  {
+    "revision": "49ae83f3517b321afc91",
+    "url": "/tic-tac-toe-game/static/js/runtime-main.d48d7d05.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9103403265e5ec1de608e4d3982a7971.js"
+  "/tic-tac-toe-game/precache-manifest.b7c34c70ea9e2e3e602485463b684637.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tic-tac-toe-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
